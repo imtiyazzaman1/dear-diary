163841c150e029d4bc1b7c11b06609d6f870dc1e
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-24 11:35:03 +0100

    #.all returns entry titles from DB

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -1,7 +1,20 @@
+require 'pg'
+
 class DiaryEntry
   attr_reader :title
-  
+
   def initialize(title)
     @title = title
+  end
+
+  def self.all
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'diary_test')
+    else
+      connection = PG.connect(dbname: 'diary')
+    end
+
+    result = connection.exec('SELECT * FROM diary_entries')
+    result.map { |entry| entry['title'] }
   end
 end
--- spec/units/diary_entry_spec.rb
+++ spec/units/diary_entry_spec.rb
@@ -4,4 +4,19 @@ describe DiaryEntry do
     entry = described_class.new('Entry 1')
     expect(entry.title).to eq('Entry 1')
   end
+
+  describe '#self.all' do
+    it "should return the title of each diary entry" do
+      connection = PG.connect(dbname: 'diary_test')
+
+      connection.exec("INSERT INTO diary_entries (title)
+        VALUES ('Entry 1'),
+          ('Entry 2'),
+          ('Entry 3');")
+
+      expect(DiaryEntry.all).to include ('Entry 1')
+      expect(DiaryEntry.all).to include ('Entry 2')
+      expect(DiaryEntry.all).to include ('Entry 3')
+    end
+  end
 end
