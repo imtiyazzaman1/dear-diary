652480c8e6563f668b11b18bee83e33822810455
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-24 11:35:27 +0100

    user can view diary entries from db

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
+require './lib/diary_entry'
 require 'sinatra/base'
 
 class Diary < Sinatra::Base
 
   get '/' do
     'Diary'
+  end
+
+  get '/diary' do
+    @entries = DiaryEntry.all.join(" ")
+    erb :diary_entries
   end
 
 end
--- spec/features/viewing_entry_titles_spec.rb
+++ spec/features/viewing_entry_titles_spec.rb
@@ -1,6 +1,14 @@
 feature 'Viewing diary entry titles' do
   scenario 'visiting the index page' do
-    visit '/'
-    expect(page).to have_content 'Diary'
+    connection = PG.connect(dbname: 'diary_test')
+    connection.exec("INSERT INTO diary_entries (title)
+      VALUES ('Entry 1'),
+        ('Entry 2'),
+        ('Entry 3');")
+
+    visit '/diary'
+    expect(page).to have_content 'Entry 1'
+    expect(page).to have_content 'Entry 2'
+    expect(page).to have_content 'Entry 3'
   end
 end
