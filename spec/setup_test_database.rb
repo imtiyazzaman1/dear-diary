0321e348df814502057ccf608d816f4f565bf07d
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-24 11:08:31 +0100

    updated testing environment

---
+++ spec/setup_test_database.rb
@@ -0,0 +1,8 @@
+require 'pg'
+
+def setup_test_database
+  p 'setting up the database'
+
+  connection = PG.connect(dbname: 'diary_test')
+  connection.exec("TRUNCATE diary_entries;")
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,11 @@ require './app'
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'setup_test_database'
 require 'simplecov'
 require 'simplecov-console'
+
+ENV['ENVIRONMENT'] = 'test'
 
 Capybara.app = Diary
 
@@ -11,3 +14,9 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
   SimpleCov::Formatter::Console
 )
 SimpleCov.start
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
