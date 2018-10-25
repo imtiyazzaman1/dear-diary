e9170a863c5061ada34b9a7172f5257e06c41a40
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-25 12:16:47 +0100

    added functionality to update an existing entry

--- app.rb
+++ app.rb
@@ -13,11 +13,11 @@ class Diary < Sinatra::Base
   end
 
   get '/view_entry' do
+    p params[:id]
     entries = DiaryEntry.all
-    entry = entries.select { |entry|
+    @entry = entries.select { |entry|
       entry.id == params[:id]
-    }
-    @entry = entry.reduce
+    }.reduce
     erb :view_entry
   end
 
@@ -25,13 +25,22 @@ class Diary < Sinatra::Base
     erb :write_entry
   end
 
+  get '/update_entry' do
+    p params
+    @entry = DiaryEntry.all.select { |entry|
+      entry.id == params[:id]
+    }.reduce
+    p @entry
+    erb :update_entry
+  end
+
   post '/save_entry' do
-    DiaryEntry.create(params[:title], params[:body])
+    DiaryEntry.update(params[:id], params[:title], params[:body]) if params[:id] != nil
+    DiaryEntry.create(params[:title], params[:body]) if params[:id] == nil
     redirect to '/diary'
   end
 
   post '/delete_entry' do
-    p params
     DiaryEntry.delete(params[:id])
     redirect to '/diary'
   end
---
+++ spec/features/updating_an_entry_spec.rb
@@ -0,0 +1,19 @@
+feature "Updating an entry" do
+  scenario "by clicking on the 'Update Entry' button" do
+    visit '/'
+    click_button 'Add Entry'
+    fill_in :title, with: 'Entry 8'
+    fill_in :body, with: 'This is entry 7'
+    click_button 'Save'
+
+    click_link 'Entry 8'
+    click_link 'Update Entry'
+    fill_in :body, with: 'Correction! This is entry 8'
+    click_button 'Save'
+
+    click_link 'Entry 8'
+    expect(page).to have_content "Entry 8\nCorrection! This is entry 8"
+
+
+  end
+end
