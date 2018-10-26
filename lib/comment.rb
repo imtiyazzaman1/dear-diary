3f4a314e3ef87119ecddbaa9cc409cb7648bc6ae
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-26 06:57:23 +0100

    added ::add method which adds a comment to the database

--- lib/comment.rb
+++ lib/comment.rb
@@ -6,4 +6,20 @@ class Comment
     @text = text
     @entry_id = entry_id
   end
+
+  def self.all(entry_id)
+    result = Database.connect.exec("SELECT * FROM comments
+      WHERE entry_id = '#{entry_id}'"
+    )
+    result.map { |comment|
+        Comment.new(id: comment['id'],
+          text: comment['text'],
+          entry_id: comment['entry_id'])
+    }
+  end
+
+  def self.add(text:, entry_id:)
+    Database.connect.exec("INSERT INTO comments (text, entry_id)
+      VALUES ('#{text}', '#{entry_id}')")
+  end
 end
--- spec/units/comment_spec.rb
+++ spec/units/comment_spec.rb
@@ -1,6 +1,14 @@
 require 'comment'
 describe Comment do
-  subject { described_class.new(id: '1', text: 'This is comment 1, linked to entry 2', entry_id: '2')}
+
+  before(:each) {
+    Database.connect.exec("INSERT INTO diary_entries (id, title, body)
+      VALUES ('1','Entry 1', 'This is entry 1');"
+    )
+  }
+
+  subject { described_class.new(id: '1', text: 'This is comment 1, linked to entry 2', entry_id: '2') }
+
   it "should have an id" do
     expect(subject.id).to eq '1'
   end
@@ -9,5 +17,24 @@ describe Comment do
   end
   it "should have an entry id" do
     expect(subject.entry_id).to eq '2'
+  end
+
+  describe '::all' do
+    it "should return all of the comments related to a diary entry " do
+
+      Database.connect.exec("INSERT INTO comments (text, entry_id) VALUES ('This is the first comment for entry 1', '1');"
+      )
+
+      expect(Comment.all('1')[0].text).to eq 'This is the first comment for entry 1'
+      expect(Comment.all('1')[0].entry_id).to eq '1'
+    end
+  end
+
+  describe '::add' do
+    it "should add a new comment to the database" do
+      Comment.add(text: 'This is another comment for entry 1', entry_id: '1')
+
+      expect(Comment.all('1')[0].text).to eq 'This is another comment for entry 1'
+    end
   end
 end
