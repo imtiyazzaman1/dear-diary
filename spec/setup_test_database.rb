84c9bb0ab67624aa46a5e6ecadd3b5c8829a60e8
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-26 07:08:25 +0100

    updated to also truncate the comments table

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -4,5 +4,6 @@ def setup_test_database
   p 'setting up the database'
 
   connection = PG.connect(dbname: 'diary_test')
-  connection.exec("TRUNCATE diary_entries;")
+  connection.exec("TRUNCATE diary_entries, comments;")
+
 end
